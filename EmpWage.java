6685b060f74500d828b4c1622a9d5ede567f69ec
ramana <ramanarajupyla@gmail.com>
2020-11-17 19:13:10 +0530

    Adding UC1 file

--- EmpWage.java
+++ EmpWage.java
@@ -1,6 +1,14 @@
 public class EmpWage {
 	public static void main(String args[]) {
 		System.out.println("***Welcome to EmpWage Details***");
+				// Constants
+		int IS_FULL_TIME = 1;
+				// Computation
+		double empcheck = Math.floor(Math.random() * 10) % 2;
+		if(empcheck == IS_FULL_TIME)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Abscent");
 	}
 }
 
