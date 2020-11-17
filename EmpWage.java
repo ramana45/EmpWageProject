7ffef33dfb171c6b6bd6d916d8341f0695e0f9eb
ramana <ramanarajupyla@gmail.com>
2020-11-17 21:04:46 +0530

    Adding UC4 file

--- EmpWage.java
+++ EmpWage.java
@@ -1,31 +1,29 @@
 public class EmpWage {
+
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
+	public static final int EMP_RATE_PER_HR = 20;
+	
 	public static void main(String args[]) {
 		System.out.println("***Welcome to EmpWage Details***");
-				// Constants
-		int IS_PART_TIME = 1;
-		int IS_FULL_TIME = 2;
-		int EMP_RATE_PER_HR = 20;
-			// Variables
+		// Variables
 		int empHrs = 0;
 		int empWage = 0;
-				// Computation
-		double empcheck = Math.floor(Math.random() * 10) % 3;
-		if (empcheck == IS_PART_TIME)
-			{
+		// Computation
+		int empcheck = (int) Math.floor(Math.random() * 10) % 3;
+		switch (empcheck) {
+			case IS_PART_TIME:
 				empHrs = 4;
 				System.out.println("Employee is Present");
-			}
-		else if (empcheck == IS_FULL_TIME)
-			{
+				break;
+			case IS_FULL_TIME:
 				empHrs = 8;
-				System.out.println("Employee is Present");
-			}
-								
-		else
-			{
+				System.out.println("Employee is Present");		
+				break;
+			default:
 				empHrs = 0;
 				System.out.println("Employee is Abscent");
-			}
+		}
 		empWage = empHrs * EMP_RATE_PER_HR;
 		System.out.println("Emp Wage: "+empWage);
 	}
