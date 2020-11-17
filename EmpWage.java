0a209e4c84a5fc3c43773a4d74f65a302bb9eb5b
ramana <ramanarajupyla@gmail.com>
2020-11-17 21:45:26 +0530

    Adding UC6 file

--- EmpWage.java
+++ EmpWage.java
@@ -4,13 +4,15 @@ public class EmpWage {
 	public static final int IS_FULL_TIME = 2;
 	public static final int EMP_RATE_PER_HR = 20;
 	public static final int NUM_OF_WORKING_DAYS = 2;
+	public static final int MAX_HRS_IN_MONTH = 10;
 
 	public static void main(String args[]) {
 		System.out.println("***Welcome to EmpWage Details***");
 		// Variables
-		int empHrs = 0, empWage = 0, totalEmpWage = 0;
+		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
 		// Computation
-		for(int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+		while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+			totalWorkingDays++;
 			int empcheck = (int) Math.floor(Math.random() * 10) % 3;
 			switch (empcheck) {
 				case IS_PART_TIME:
@@ -19,18 +21,18 @@ public class EmpWage {
 					break;
 				case IS_FULL_TIME:
 					empHrs = 8;
-					System.out.println("Employee is Present");		
+					System.out.println("Employee is Present");	
 					break;
 				default:
 					empHrs = 0;
 					System.out.println("Employee is Abscent");
 			}
-
-		empWage = empHrs * EMP_RATE_PER_HR;
-		totalEmpWage += empWage;
-		System.out.println("Emp Wage: "+empWage);
+		totalEmpHrs += empHrs;
+		System.out.println("Day#: " +totalWorkingDays + " Emp Hr: " +empHrs);
 		}
-		System.out.println("Total Emp Wage: "+totalEmpWage);
+	int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HR;
+	System.out.println("Total Emp Wage: "+totalEmpWage);
 	}
 }
+
 
