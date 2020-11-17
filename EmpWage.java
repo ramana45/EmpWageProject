f1cd7c47133b5ae075b1ac307442d8f2a768d192
ramana <ramanarajupyla@gmail.com>
2020-11-17 19:33:19 +0530

    Adding UC2 file

--- EmpWage.java
+++ EmpWage.java
@@ -3,12 +3,25 @@ public class EmpWage {
 		System.out.println("***Welcome to EmpWage Details***");
 				// Constants
 		int IS_FULL_TIME = 1;
+		int EMP_RATE_PER_HR = 20;
+			// Variables
+		int empHrs = 0;
+		int empWage = 0;
 				// Computation
 		double empcheck = Math.floor(Math.random() * 10) % 2;
-		if(empcheck == IS_FULL_TIME)
-			System.out.println("Employee is Present");
+		if (empcheck == IS_FULL_TIME)
+			{
+				empHrs = 8;
+				System.out.println("Employee is Present");
+			}
+				
 		else
-			System.out.println("Employee is Abscent");
+			{
+				empHrs = 0;
+				System.out.println("Employee is Abscent");
+			}
+		empWage = empHrs * EMP_RATE_PER_HR;
+		System.out.println("Emp Wage: "+empWage);
 	}
 }
 
